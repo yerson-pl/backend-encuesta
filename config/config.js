f19c5ad5e32ad09c0c9adaa538939fe17729f264
yerson <yerson.pl@gmail.com>
2018-10-24 10:49:44 -0500

    ultima configuracion mongo

--- config/config.js
+++ config/config.js
@@ -14,12 +14,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 
 //Base de datos
-//let urlDB;
-//if( process.env.NODE_ENV === 'dev'){
-//    urlDB = 'mongodb://localhost:27017/encuestaDB';
-//}else{
+let urlDB;
+if( process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/encuestaDB';
+}else{
     urlDB = 'mongodb://user-encuesta:encuesta1234@ds141043.mlab.com:41043/encuesta-db'
-//}
+}
 
 process.env.URLDB = urlDB;
 
